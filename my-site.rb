55d5b9e380842435ba07aebd1ec221a1aff17543
Jennie Buechner <janina.buechner@gmail.com>
2015-10-27 16:41:00 -0700

    create layout template

--- my-site.rb
+++ my-site.rb
@@ -3,18 +3,22 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
+    @title = 'My Homepage'
     erb :index
   end
 
   get "/about" do
+    @title = 'About Jennie'
     erb :about
   end
 
   get "/blog" do
+    @title = 'My Blog'
     erb :blog
   end
 
   get "/project" do
+    @title = 'My Projects'
     erb :project
   end
 end
